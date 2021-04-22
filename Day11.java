dc6e4f4b07fbe8b5c004388111bb978c010451dd
疯疯癫癫 <372715403@qq.com>
2021-04-22 22:43:01 +0800

    求最近公共祖先 求bit数字二进制中最大连续的1的个数

---
+++ Day11.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+/**
+ * Crtated with IntelliJ IDEA.
+ * Destcription:
+ * User: hp
+ * Date: 2021-04-22
+ * Time: 22:21
+ */
+public class Day11 {
+    /*求最大连续bit数中1的个数
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        byte n =in.nextByte();
+        int max = 0 ;
+        int count = 0;
+        for (int i=0;i<=7;i++){
+            if(((n>>i)&1)== 1){
+                count++;
+
+            }else{
+                max = Math.max(max,count);
+                count = 0;
+            }
+        }
+        max = Math.max(max,count);
+        System.out.println(max);
+    }
+
+
+
+
+
+    求最近公共祖先
+    import java.util.*;
+
+public class LCA {
+    public int getLCA(int a, int b) {
+        // write code here
+        while(a!=b){
+            for( ;a<b;b/=2);
+            for( ;a>b;a/=2);
+        }
+        return a;
+    }
+
+}
+
+     */
+
+
+
+}
