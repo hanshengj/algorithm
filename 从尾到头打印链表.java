361ec8820a5aa7a0fbc74bdef566cb46fd41bf23
疯疯癫癫 <372715403@qq.com>
2021-04-30 22:40:26 +0800

    从尾到头打印链表

---
+++ 从尾到头打印链表.java
@@ -0,0 +1,19 @@
+
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+class Solution {
+    ArrayList<Integer> tmp = new ArrayList<Integer>();
+    public int[] reversePrint(ListNode head) {
+        recur(head);
+        int[] res = new int[tmp.size()];
+        for(int i = 0; i < res.length; i++)
+            res[i] = tmp.get(i);
+        return res;
+    }
+    void recur(ListNode head) {
+        if(head == null) return;
+        recur(head.next);
+        tmp.add(head.val);
+    }
+}
