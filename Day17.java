f634f155f6e425bb3c0ead79b95d2f3896bed780
疯疯癫癫 <372715403@qq.com>
2021-05-06 23:17:41 +0800

    杨辉三角的变形，长整数相加

---
+++ Day17.java
@@ -0,0 +1,57 @@
+/**
+ * Crtated with IntelliJ IDEA.
+ * Destcription:
+ * User: hp
+ * Date: 2021-05-03
+ * Time: 13:48
+ */
+
+    public class Day17  {
+      /*
+  杨辉三角的变形
+
+      import java.util.*;
+    public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+        int n=sc.nextInt();
+            System.out.println(run(n));
+
+        }
+    }
+     public static int run(int x){
+
+		 if(x<=2){//前两行直接返回-1
+			 return -1;
+		 }else if(x%2 == 1){//奇数行第一个出现偶数为2
+			 return 2;
+		 }
+         //偶数行
+	 return x / 2 % 2 + 3;
+	 }
+
+}
+
+超长正整数相加
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String str1= sc.nextLine();
+            String str2 = sc.nextLine();
+            System.out.println(AddLongInteger(str1,str2));
+        }
+    }
+
+    public static String AddLongInteger(String str1,String str2){
+            long num1=Long.valueOf(str1);
+            long num2=Long.valueOf(str2);
+            String ret= Long.toString(num1+num2);
+            return ret;
+    }
+}
+       */
+    }
+
