724f82710093b1ce221921fd18bb2792e8d6f17b
疯疯癫癫 <372715403@qq.com>
2021-06-17 21:29:21 +0800

    把数字翻译成字符串

---
+++ 把数字翻译成字符串.java
@@ -0,0 +1,24 @@
+
+给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，
+1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多
+少种不同的翻译方法。
+
+ 
+
+
+class Solution {
+    public int translateNum(int num) {
+
+     String s = String.valueOf(num);
+        int a = 1, b = 1;
+        for(int i = s.length() - 2; i > -1; i--) {
+            String tmp = s.substring(i, i + 2);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
+        }
+        return a;
+
+}
+}
